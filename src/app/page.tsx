3909d0e0cc076eaf6291039c543b8352d3a5c3e3
IvanBersenev <bersenev4dev@gmail.com>
2025-06-28 15:33:48 +0700

    no optimize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,20 @@ import { Suspense } from "react";
 export default function Home() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Suspense fallback={<Loader />}>
-        <Canvas
-          camera={{
-            fov: 45,
-            near: 0.1,
-            far: 20000, // Увеличиваем дальность прорисовки
-            position: [3000, 3000, 3000], // Позиция камеры (x, y, z)
-          }}
-        >
-          <ambientLight intensity={0.5} />
-          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-          <pointLight position={[-10, -10, -10]} />
-          <Model url="/krsk.glb" />
-          <OrbitControls />
-        </Canvas>
-      </Suspense>
+      <Canvas
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 20000, // Увеличиваем дальность прорисовки
+          position: [3000, 3000, 3000], // Позиция камеры (x, y, z)
+        }}
+      >
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <Model url="/krsk.glb" />
+        <OrbitControls />
+      </Canvas>
     </div>
   );
 }
